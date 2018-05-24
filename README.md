3b0ff345840a55434166e1876eadeba073a288e2
souky <136069803@qq.com>
2018-05-24 22:35:50 +0800

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# soukys
-404 is killing me , please help
--- src/main/java/com/souky/common/utils/JsonRsultCode.java
+++
@@ -1,7 +0,0 @@
-package com.souky.common.utils;
-
-public class JsonRsultCode {
-
-	public static final String codeSuccess="1000";
-	public static final String codeError="2000";
-}
